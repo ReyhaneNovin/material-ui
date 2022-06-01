0f5f838bbfdff25dd7f434df8cae94a4a920548b
r.novin <r.novin@ebcom.ir>
2022-06-01 14:04:55 +0430

    estimate Page

--- src/components/Estimate.jsx
+++ src/components/Estimate.jsx
@@ -341,6 +341,12 @@ function Estimate() {
 
   const [message, setMessage] = useState('');
   const [total, setTotal] = useState(0);
+  const [service, setService] = useState([]);
+  const [platforms, setPlatforms] = useState([]);
+  const [features, setFeatures] = useState([]);
+  const [customFeatures, setCustomFeatures] = useState('');
+  const [category, setCategory] = useState('');
+  const [users, setUsers] = useState('');
 
   const defaultOptions = {
     loop: true,
@@ -410,12 +416,15 @@ function Estimate() {
     switch (newSelected.title) {
       case 'Custom Software Development':
         setQuestions(softwareQuestions);
+        setService(newSelected.title);
         break;
       case 'iOS/Android App Development':
         setQuestions(softwareQuestions);
+        setService(newSelected.title);
         break;
       case 'Website Development':
         setQuestions(websiteQuestions);
+        setService(newSelected.title);
         break;
 
       default:
@@ -456,13 +465,43 @@ function Estimate() {
     if (questions.length > 2) {
       const userCost = questions
         .filter((question) => question.title === 'How many users di you ecpect?')
-        .map((question) => question.options.filter((option) => option.selected))[0][0].cost;
-      cost -= userCost;
-      cost += userCost;
+        .map((question) => question.options.filter((option) => option.selected))[0][0];
+      setUsers(userCost.title);
+      cost -= userCost.cost;
+      cost *= userCost.cost;
 
       console.log(cost);
     }
     setTotal(cost);
+  };
+  const getPlatforms = () => {
+    let newPlatforms = [];
+    if (questions.length > 2) {
+      questions
+        .filter((question) => question.title === 'wich platforms do you need?')
+        .map((question) => question.options.filter((option) => option.selected))[0]
+        .map((option) => newPlatforms.push(option.title));
+      console.log(newPlatforms);
+      setPlatforms(newPlatforms);
+    }
+  };
+  const getFeatures = () => {
+    let newFeatures = [];
+    if (questions.length > 2) {
+      questions
+        .filter((question) => question.title === 'wich features do you need?')
+        .map((question) => question.options.filter((option) => option.selected))
+        .map((option) => option.map((newFeatures) => newFeatures.push(newFeatures.title)));
+      setFeatures(newFeatures);
+    }
+  };
+  const getCustomFeatures = () => {
+    if (questions.length > 2) {
+      const newCustomFeatures = questions
+        .filter((question) => question.title === 'wich CustomFeatures do you need?')
+        .map((question) => question.options.filter((option) => option.selected))[0][0].title;
+      setCustomFeatures(newCustomFeatures);
+    }
   };
   return (
     <Grid container direction='row'>
@@ -579,6 +618,8 @@ function Estimate() {
             onClick={() => {
               setDialogOpen(true);
               getTotal();
+              getPlatforms();
+              getFeatures();
             }}
           >
             Get estimate
@@ -595,7 +636,7 @@ function Estimate() {
         </Grid>
         <DialogContent>
           <Grid container>
-            <Grid item container direction='column'>
+            <Grid item container direction='column' md={7}>
               <Grid item style={{ marginBottom: '0.5em' }}>
                 <TextField
                   id='name'
@@ -654,6 +695,97 @@ function Estimate() {
                 <Typography variant='body1' paragraph>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, consequatur rerum.
                 </Typography>
+              </Grid>
+            </Grid>
+            <Grid item container direction='column' md={5}>
+              <Grid item>
+                <Grid container direction='column'>
+                  <Grid item container alignItems='center'>
+                    <Grid item>
+                      <img src={check} alt='check' />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant='body1'>
+                        You want {service}
+                        {platforms.length > 0
+                          ? `for ${
+                              //if only web application is selected...
+                              platforms.indexOf('Web Application') > -1 && platforms.length === 1
+                                ? //then finish sentence here
+                                  'a Web Application.'
+                                : //otherwise, if web application and another platform is selected...
+                                platforms.indexOf('Web Application') > -1 && platforms.length === 2
+                                ? //then finish the sentence here
+                                  `a Web Application and an ${platforms[1]}.`
+                                : //otherwise, if only one platform is selected which isn't web application...
+                                platforms.length === 1
+                                ? //then finish the sentence here
+                                  `an ${platforms[0]}`
+                                : //otherwise, if other two options are selected...
+                                platforms.length === 2
+                                ? //then finish the sentence here
+                                  'an iOS Application and an Android Application.'
+                                : //otherwise if all three are selected...
+                                platforms.length === 3
+                                ? //then finish the sentence here
+                                  'a Web Application, an iOS Application, and an Android Application.'
+                                : null
+                            }`
+                          : null}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container alignItems='center'>
+                    <Grid item>
+                      <img src={check} alt='check' />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant='body1'>
+                        {' '}
+                        {'with '}
+                        with GPS ,Photo/Video and File Transfer
+                        {/* if we have features... */}
+                        {features.length > 0
+                          ? //...and there's only 1...
+                            features.length === 1
+                            ? //then end the sentence here
+                              `${features[0]}.`
+                            : //otherwise, if there are two features...
+                            features.length === 2
+                            ? //...then end the sentence here
+                              `${features[0]} and ${features[1]}.`
+                            : //otherwise, if there are three or more features...
+                              features
+                                //filter out the very last feature...
+                                .filter((feature, index) => index !== features.length - 1)
+                                //and for those features return their name...
+                                .map((feature, index) => <span key={index}>{`${feature}, `}</span>)
+                          : null}
+                        {features.length > 2
+                          ? //...and then finally add the last feature with 'and' in front of it
+                            ` and ${features[features.length - 1]}.`
+                          : null}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container alignItems='center'>
+                    <Grid item>
+                      <img src={check} alt='check' />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant='body1'>
+                        The custom Features {customFeatures.toLowerCase()}
+                        {`,and the project will be used by about ${users} users.`}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Button variant='contained' className={classes.estimateButton}>
+                  Place Request
+                  <img src={send} alt='airplane' style={{ marginLeft: '0.5em' }} />
+                </Button>
               </Grid>
             </Grid>
           </Grid>
